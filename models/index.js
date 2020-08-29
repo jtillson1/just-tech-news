2a07d9cd3986776a854d99ae4a9a6433d6852176
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-08-29 13:47:40 -0500

    post-routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,16 @@
 const User = require('./User');
 const Post = require("./Post");
 
+//create associations
+// This association creates the reference for the id column in the User model to link to the corresponding foreign key pair, which is the user_id in the Post model.
+User.hasMany(Post, {
+    foreignKey: 'user-id'
+});
+
+//a post can belong to one user, but not many users
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+
 module.exports = { User, Post };
