70d10cbc4dc4bfe5b980c940c2af81ca6597e447
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-08-30 17:17:21 -0500

    comment model

--- models/index.js
+++ models/index.js
@@ -1,44 +1,57 @@
 const User = require('./User');
 const Post = require("./Post");
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
+//user model associations
 //create associations
 // This association creates the reference for the id column in the User model to link to the corresponding foreign key pair, which is the user_id in the Post model.
 User.hasMany(Post, {
     foreignKey: 'user-id'
 });
 
-//a post can belong to one user, but not many users
+//post model associations
 Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
-
 User.belongsToMany(Post, {
     through: Vote,
     as: 'voted_posts',
     foreignKey: 'user_id'
-  });
-  
-  Post.belongsToMany(User, {
+});
+Post.belongsToMany(User, {
     through: Vote,
     as: 'voted_posts',
     foreignKey: 'post_id'
-  });
+});
 
 
+//vote model associations
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+Vote.belongsTo(Post, {
+    foreignKey: 'user_id'
+});
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
 
+//comment model associations
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
 
-  Vote.belongsTo(User, {
-      foreignKey: 'user_id'
-  });
-  Vote.belongsTo(Post, {
-      foreignKey: 'user_id'
-  });
-  User.hasMany(Vote, {
-      foreignKey: 'user_id'
-  });
-  Post.hasMany(Vote, {
-      foreignKey: 'post_id'
-  });
-
-module.exports = { User, Post, Vote };
+module.exports = { User, Post, Vote, Comment };
