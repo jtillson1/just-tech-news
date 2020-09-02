10b7c93b07831b5c2998ad693c6b8d15061bc313
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-09-02 17:46:55 -0500

    login is persistent on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
-
-
+//render homepage 
 router.get('/', (req, res) => {
+    console.log(req.session);
     Post.findAll({
         attributes: [
             'id',
@@ -40,7 +40,15 @@ router.get('/', (req, res) => {
         });
 });
 
-
+//render login page
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,21 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 // set up Handlebars.js as your app's template engine of choice
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -16,9 +31,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 // turn on routes
 app.use(routes);
 
+
 // turn on connection to db and server
 //The "sync" part means that this is Sequelize taking the models and connecting them to associated database tables. If it doesn't find a table, it'll create it for you!
-
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
